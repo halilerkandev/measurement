4f63380f88d64608e19743cdcfd5bf1fba9a1960
Halil Erkan Dursunoğlu <h.erkan.dursunoglu@gmail.com>
2020-09-21 04:29:32 +0300

    fixed a bug with ingress-nginx on Digital Ocean

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
+// DEV
+// baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local'
+
 const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'www.measurement-app-prod.site/',
       headers: req.headers,
     });
   } else {
